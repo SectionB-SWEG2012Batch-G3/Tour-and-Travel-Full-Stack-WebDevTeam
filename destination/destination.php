1dcb3dddf713d0c8a7c151c174bde8993ea2706c
Haymanot-Demis <88848202+Haymanot-Demis@users.noreply.github.com>
2022-05-14 08:40:15 +0300

    user pages added for hotel and dest

---
+++ destination/destination.php
@@ -0,0 +1,447 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>4HF Tour & Travels| Addis Ababa</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="places to visit in Addis ababa">
+	<meta name="keywords" content="entoto, unity park,sheger park, zoma museum">
+	<link rel="stylesheet" type="text/css" href="css/style1.css">
+	<link rel="stylesheet" href="../css/footerCSS.css">
+	<link rel="stylesheet" href="../css/navStyle.css">
+	<link rel="stylesheet" href="../css/HomepageCss.css">
+	<script defer src="../JS/NavScript.js"></script>
+	<script defer src="../JS/search-boxScript.js"></script>
+	<link rel="stylesheet" href="../fontawesome-free-5.15.4-web/css/all.min.css">
+</head>
+<body id="body">
+<header>
+		<div class="top2">
+			<div class="logo" style="height: 100px;width: 125px;">
+				<a href="../../Homepage.html">
+					<svg height="100" width="125">
+						<ellipse cx="62.5" cy="50" rx="55" ry="45" fill="url(#grad2)"/>
+						<polygon points="62.5,5 85,50 62.5,95 40,50" style="stroke: rgb(255, 255, 255); fill: rgba(255, 255, 0, 1);"/>
+						<text fill ="#000000" font-size="25" font-family="Verdana"
+							x="20" y="60">4</text>
+							<text fill ="#000000" font-size="25" font-family="Verdana"
+							x="52.5" y="60">H</text>
+							<text fill ="#000000" font-size="25" font-family="Verdana"
+							x="90" y="60">F</text>
+							<text fill ="#000000" font-size="13" font-family="Verdana"
+							x="20" y="80">Tour &amp; Travel</text>
+							
+						<defs>
+							<linearGradient id="grad2" x1="0%" y1="0%" x2="50%" y2="0%" x3="51%" y3="0%" x4="100%" y4="100%">
+								<stop offset="0%" style="stop-color: rgb(0, 255, 0);stop-opacity: 1;"/>
+								<stop offset="100%" style="stop-color: rgb(255, 255, 0);stop-opacity: 1;"/>
+								<stop offset="100%" style="stop-color: rgb(255, 255, 0);stop-opacity: 1;"/>
+								<stop offset="200%" style="stop-color: rgb(255, 0, 0); stop-opacity: 1;"/>
+							</linearGradient>
+						</defs>
+						
+					</svg>
+				</a>
+			</div>
+			<div class = "login-container">
+				<button class = "blogs" ><a href = "Travel logs/Tips.html" >Travel Blogs</a></button>
+				<button class = "login" style = "width:100px;"><a href = "../../tour guide/Profiles/log in.html">Log In</a></button>    
+			</div>
+			<div  class = "search-bar-container">
+				<div class="input-container">
+					<input class = "searchBar" type = "Search" name = "searchBar" value = "" placeholder = "Where to go... " list = "ethiopia">
+					<a href = "#" class = "search-icon">
+						<i class="fa fa-2x fa-search" aria-hidden="true"></i>
+					</a>
+				</div>
+				
+				<div class = "autocom-box" style="margin-left: 10px;">
+					<ul>
+						<li></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	<nav>
+    <label for="fa-bars-label" class = "fa-bars-label" onclick="changeFont()">
+        <i class="fas fa-bars"></i>
+    </label>
+    <input type="checkbox" id = "fa-bars-label">
+    <ul>
+        <li><a href="../../Homepage.html">Home</a></li>
+        <li>
+            <label for="dest-label" class="show">Destinations+</label>
+            <a href="../../destination/destinations.html">Destinations</a>
+            <input type="checkbox" id = "dest-label">
+            <ul>
+                <li>
+                    <label for="addis-label" style = "margin-left: 50px;" class="show">Addis A.+</label>
+                    <a href = "addisababa.html">Addis Ababa</a>
+                    <input type="checkbox" id = "addis-label">
+                    <ul>
+                        <li><a href="mountentoto.html">Entoto</a></li>
+                        <li><a href="unitypark.html">Unity Park</a></li>
+                        <li><a href="friendshippark.html">Friendship Park</a></li>
+                        <li><a href="zomamuseum.html">Zoma Museum</a></li>
+                    </ul>
+                </li>
+                <li>
+                	  <label for="Bahir-label" style = "margin-left: 50px;" class="show">Bahirdar+</label>
+                    <a href = "../bahirdar/bahirdar.html">Bahirdar</a>
+                    <input type="checkbox" id = "Bahir-label">
+                    <ul>
+                        <li><a href="../bahirdar/laketana.html">Lake Tana</a></li>
+                        <li><a href="../bahirdar/azwamariam.html">Azwa Mariam</a></li>
+                        
+                    </ul>
+                </li>
+
+
+                <li><a href = "../harar/harar.html">Harar</a> </li>
+                <li><a href = "../gonder/gonder.html">Gonder</a></li>
+                <li><a href = "../afar/afar.html">Afar</a></li>
+                <li><a href = "../jinka/jinka.html">Jinka</a></li>
+            </ul>
+        </li>
+        <li>
+            <label for="hotel-label" class="show">Hotel+</label>
+            <a href="../../Hotel Reservation/hotel reservation.html">Hotel Reservation</a>
+            <input type="checkbox" id = "hotel-label">
+            <ul >
+                <li><a href = "../../Hotel Reservation/Sheraton Addis/Sheraton Addis.html">Sheraton</a></li>
+                <li><a href = "../../Hotel Reservation/Skylight/Skylight.html">Skylight</a></li>
+                <li><a href = "../../Hotel Reservation/Golden Tulip/Golden Tulip.html">Golden Tulip</a></li>
+                <li><a href = "../../Hotel Reservation/Hyatt/Hyatt.html">Hyatt</a></li>
+                <li><a href = "../../Hotel Reservation/Marriott/Marriott.html">Mariott</a></li>
+                <li><a href = "../../Hotel Reservation/Emerald/Emerald Lodge.html">Emerald</a></li>
+            </ul>
+        </li>
+        <li>
+            <label for="transport-label" class="show">Transport+</label> 
+            <a href="../../transportation/transport.html">Transport</a>
+            <input type="checkbox" id = "transport-label">
+            <ul>
+                <li><a href = "../../transportation/transport.html">For family</a></li>
+                <li><a href = "../../transportation/transport.html">For Alone</a></li>
+                <li><a href = "../../transportation/transport.html">For Group</a></li>
+            </ul>
+        </li>
+        <li class="tour-guide"><a href="../../tour guide/tourguide.html">Tour Guide</a></li>
+        <li class="Experience"><a href="#" >Experience</a></li> 
+        <li class="About-us"><a href="../../About us/About us.html">About us</a></li>
+    </ul>
+</nav>
+</header>
+
+ <main>	
+
+		<h1>Addis Ababa</h1>
+
+
+		<div class="slideContainer">
+
+			<div class="slider">
+				<div class="slide active">
+					<img src="images/addis2.jpg" alt="Picture of Addis Ababa">
+					<div class="info">
+						<h2>Addis Ababa</h2>
+						<p></p>
+					</div>
+				</div>
+	
+				<div class="slide">
+					<img src="images/addis3.webp" alt="Picture of Addis Ababa">
+					<div class="info">
+						<h2>Addis Ababa</h2>
+						<p></p>
+					</div>
+				</div>
+	
+				<div class="slide">
+					<img src="images/entoto.jpg " alt="Picture of Entoto Park">
+					<div class="info">
+						<h2>Entoto Park</h2>
+						<p></p>
+					</div>
+				</div>
+	
+				<div class="slide">
+					<img src="images/unity.jpg " alt="Picture of Unity Park">
+					<div class="info">
+						<h2>Unity Park</h2>
+						<p></p>
+					</div>
+				</div>
+	
+				<div class="slide">
+					<img src="images/friendship.jpg " alt="Picture of Friendship Park">
+					<div class="info">
+						<h2>Friendship Park</h2>
+						<p></p>
+					</div>
+				</div>
+	
+				<div class="slide">
+					<img src="images/zoma3.jpg " alt="Picture of Zoma Museum">
+					<div class="info">
+						<h2>Zoma Museum</h2>
+						<p></p>
+					</div>
+				</div>
+
+				
+	
+				<div class="navigation">
+
+					<svg height="50" width="50" class="prev-btn">
+						<polygon points="10,25 40,5 40,15 25,25 40,35 40,45 "
+							style="stroke: rgb(255, 255, 255); fill: rgb(0, 0, 0);" />
+					</svg>
+					<svg height="50" width="50" class="next-btn">
+						<polygon points="10,5 40,25 10,45 10,35 25,25 10,15 "
+							style="stroke: rgb(255, 255, 255); fill: rgb(0, 0, 0);" />
+					</svg>
+				</div>
+				<div class="navigation-visibility">
+					<div class="slide-icon active">
+						<svg height="20" width="30">
+							<polygon class="slideSvg" points="1,1 29,1 29,15 1,15" style="stroke: rgb(255, 255, 255); " />
+						</svg>
+					</div>
+					<div class="slide-icon">
+						<svg height="20" width="30">
+							<polygon class="slideSvg" points="1,1 29,1 29,15 1,15" style="stroke: rgb(255, 255, 255); " />
+						</svg>
+					</div>
+					<div class="slide-icon">
+						<svg height="20" width="30">
+							<polygon class="slideSvg" points="1,1 29,1 29,15 1,15" style="stroke: rgb(255, 255, 255); " />
+						</svg>
+					</div>
+					<div class="slide-icon">
+						<svg height="20" width="30">
+							<polygon class="slideSvg" points="1,1 29,1 29,15 1,15" style="stroke: rgb(255, 255, 255); " />
+						</svg>
+					</div>
+					<div class="slide-icon">
+						<svg height="20" width="30">
+							<polygon class="slideSvg" points="1,1 29,1 29,15 1,15" style="stroke: rgb(255, 255, 255); " />
+						</svg>
+					</div>
+					<div class="slide-icon">
+						<svg height="20" width="30">
+							<polygon class="slideSvg" points="1,1 29,1 29,15 1,15" style="stroke: rgb(255, 255, 255);" />
+						</svg>
+					</div>
+					
+				</div>
+			</div>
+	
+	
+	
+		</div>
+
+		<div class="mainParagraph scroll">
+			<p>
+				Addis Ababa,capital and largest city of Ethiopia is located
+				on a well-watered plateau surrounded by hills and mountains
+				in the geographic centre of the country. Home to the headquarters of
+		       	the African Union, the third largest United Nations population, and with
+			    representation from nearly every foreign embassy from around the world,
+				Addis Ababa is often referred to as the political capital of Africa.
+				<div class="tap">
+					<button class="button"><a class="tap-here" href="https://en.wikipedia.org/wiki/Addis_Ababa " target="_blank" > Tap Here </a></button> to read More about Addis Ababa.
+				 </div>
+			</p>
+		</div>
+
+
+		
+		<h2 class="scroll">Places To Visit in Addis Ababa</h2>
+		<div class="alone-grid">
+            <div class="alone-grid-item scroll">
+                <div class="alone-card">
+                    <div class="imc">
+                    <img class="alone-card-img" src="images/entoto.jpg " alt="Picture of Entoto Park">
+                    </div>
+                    <div class="alone-card-content">
+                        <h2 class="alone-card-header">Mount Entoto(Entoto Park)</h2>
+                        <p class="alone-card-text">
+                            Entoto Natural Park is a park of serenity and resounding joy.
+							 It’s a unique place to discover the most breathtaking ancient
+							  mountain nature with streams flowing from clear springs and waterfalls.
+				
+                        </p>
+                        <button class="alone-card-btn"><a href="mountentoto.html">See More</a></button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="alone-grid-item scroll">
+                <div class="alone-card">
+                    <div class="imc">
+                    <img class="alone-card-img" src="images/unity.jpg " alt="Picture of Unity Park">
+                    </div>
+                    <div class="alone-card-content">
+                        <h2 class="alone-card-header">Unity Park</h2>
+                        <p class="alone-card-text">
+                            The Unity Park of Ethiopia is one of the most modern and
+							 multi-faceted tourist attractions in the country.
+							  The Ethiopian Unity Park is in the compound of the National
+							   Grand Palace, which is at Arat kilo in Addis Ababa.
+				
+                        </p>
+                        <button class="alone-card-btn"><a href="unitypark.html">See More</a></button>
+                    </div>
+                </div>
+            </div>
+
+			<div class="alone-grid-item scroll">
+                <div class="alone-card">
+                    <div class="imc">
+                    <img class="alone-card-img" src="images/friendship.jpg " alt="Picture of Friendship Park">
+                    </div>
+                    <div class="alone-card-content">
+                        <h2 class="alone-card-header">Friendship Park(Sheger Park)</h2>
+                        <p class="alone-card-text">
+                            The Addis Ababa Riverside Green Development Project, 
+							also called Beautifying Sheger, is a venture supported
+							 by the Ethiopian government and backed by Chinese government
+							  that expects to clean rivers and create public works in the capital city, Addis Ababa.
+				
+                        </p>
+                        <button class="alone-card-btn"><a href="friendshippark.html">See More</a></button>
+                    </div>
+                </div>
+            </div>
+
+			<div class="alone-grid-item scroll">
+                <div class="alone-card">
+                    <div class="imc">
+                    <img class="alone-card-img" src="images/zoma3.jpg " alt="Picture of Zoma Museum">
+                    </div>
+                    <div class="alone-card-content">
+                        <h2 class="alone-card-header">Zoma Museum</h2>
+                        <p class="alone-card-text">
+                            Zoma Museum is a dream inspired 25 years ago
+							 by the timeless and structurally sound vernacular
+							  architecture of Ethiopia and other parts of the world.
+			
+                        </p>
+                        <button class="alone-card-btn"><a href="zomamuseum.html">See More</a></button>
+                    </div>
+                </div>
+            </div>
+		</div>
+
+
+		<div class="iframe scroll">
+			<iframe src="https://www.youtube.com/embed/Q95r9__ZNeg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+			</iframe> 
+		</div>
+		<h3 class="scroll">Recommended Hotels</h3>
+		<div class="alone-grid">
+            <div class="alone-grid-item scroll">
+                <div class="alone-card">
+                    <div class="imc">
+                    <img class="alone-card-img" src="../../Hotel Reservation/Sheraton Addis/image1/sheratonaddis.jpg" alt="addis" >
+                    </div>
+                    <div class="alone-card-content">
+                        <h2 class="alone-card-header">Sheraton Addis</h2>
+                        <p class="alone-card-text">
+                            Addis Ababa,capital and largest city of Ethiopia is located
+									 on a well-watered plateau surrounded by hills and mountains in
+									  the geographic centre of the country.
+                        </p>
+                        <button class="alone-card-btn"><a href="acar7.html">See More</a></button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="alone-grid-item scroll">
+                <div class="alone-card">
+                    <div class="imc">
+                    <img class="alone-card-img" src="../../Hotel Reservation/Golden Tulip/image2/addisgold4.jpg" alt="bahir dar">
+                    </div>
+                    <div class="alone-card-content">
+                        <h2 class="alone-card-header">Golden Tulip</h2>
+                        <p class="alone-card-text">
+                            The rate is 4.3 out of 5. it is best choice for individual tourist or for couples. the price
+                            is 65$.
+                        </p>
+                        <button class="alone-card-btn"><a href="acar22.html">See More</a></button>
+                    </div>
+                </div>
+            </div>
+
+			<div class="alone-grid-item scroll">
+                <div class="alone-card">
+                    <div class="imc">
+                    <img class="alone-card-img" src="../../Hotel Reservation/Skylight/image5/addisskylight1.jpg" alt="jinka">
+                    </div>
+                    <div class="alone-card-content">
+                        <h2 class="alone-card-header">Skylight</h2>
+                        <p class="alone-card-text">
+                            The rate is 4.3 out of 5. it is best choice for individual tourist or for couples. the price
+                            is 65$.
+                        </p>
+                        <button class="alone-card-btn"><a href="acar22.html">See More</a></button>
+                    </div>
+                </div>
+            </div>
+		</div>
+</main>
+<footer>
+	<div class="container scroll">
+		<div class="row">
+			<div class="footer-items">
+				<h4>Terms and Privacy</h4>
+				<ul id = "lists" style="color:#bbb;text-decoration: none;" >
+					<li><a href="#">Faq</a></li>
+					<li><a href="../../About us/About us.html">About us</a></li>
+					<li><a href="#">Privacy policy</a></li>
+					<li>Travel informations</li>
+				</ul>
+			</div>
+			<div class="footer-items">
+				<h4>Contacts</h4>
+				<ul id = "lists">
+					<li>Tel : +251111234534</li>
+					<li><a style="color:#bbb;text-decoration: none;" href="mailto:email@gmail.com">E-mail</a></li>
+				</ul>
+			</div>
+			<div class="footer-items">
+				<h4>Follow us on</h4>
+				<div class="socials">
+					<a href="#"><i class="fab fa-youtube"></i></a>
+					<a href="#"><i class="fab fa-facebook"></i></a>
+					<a href="#"><i class="fab fa-telegram"></i></a>
+					<a href="#"><i class="fab fa-instagram"></i></a>
+				</div>
+			</div>
+			<div class = "copyright" style = "width:100%; text-align: center;color: white;">
+				<i>Copyright 2022 by Refsnes Data. All Rights Reserved.</i>
+			 </div>
+		</div>
+	</div>
+</footer>
+<div class="to-top">
+	<a href="#top">
+		<i class="fas fa-chevron-up"></i>
+	</a>
+</div>
+
+<script>
+	const toTop = document.querySelector(".to-top");
+	window.addEventListener("scroll" ,() => {
+		if(window.scrollY > 200){
+			toTop.classList.add("visible");
+		}else{
+			toTop.classList.remove("visible");
+		}
+	});
+</script>
+
+	<script src="../js/addis.js"></script>
+</body>
+</html>
